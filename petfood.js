26c4398785c5a5a8df354eba3c02678f56f46e55
duggerNot <dugger.jk@gmail.com>
2016-02-03 17:46:33 -0600

    fixed for loops to correctly pull info into dom, added functions to loop through cat json

--- petfood.js
+++ petfood.js
@@ -1,55 +1,103 @@
+console.log("First line in JS file" );
 console.log(Date.now());
 
-function executeThisCodeIfXHRFails() {
-	alert("XHR FAILED");
+
+var contentEl = document.getElementById("mainContent");
+
+
+
+function dogFoodFails() {
+	alert("DOG XHR FAILED");
+};
+
+function catFoodFails() {
+	alert("CAT XHR FAILED");
 };
 
 
-
-function executeThisCodeAfterFileIsLoaded () {
-	var data = JSON.parse(this.responseText);
-	var contentEl = document.getElementById("mainContent");
-
-
-
+function dogFoodLoaded() {
+	var dogData = JSON.parse(this.responseText);
 
 	var dogFoodData = "";
 	var currentFood = "";
 
-	for(var i = 0; i < data.dog_brands.length; i++) {
-		currentFood = data.dog_brands[i];
-		dogFoodData += `<div class='petFoodOutput'>`;
-		dogFoodData += `<h1>${data.dog_brands[i].name}</h1>`;
+	for(var i = 0; i < dogData.dog_brands.length; i++) {
+		currentFood = dogData.dog_brands[i];
+		dogFoodData += `<div class='dogFoodOutput'>`;
+		dogFoodData += `<h1 id='dogBrandName'>${currentFood.name}</h1>`;
+		// console.log("dog first for", currentFood);
 
 
-		for (var j = 0; j < data.dog_brands[i].length; j++) {
-			dogFoodData += `<p>${currentFood.types[j].type}</p>`;
+		for (var j = 0; j < dogData.dog_brands[i].types.length; j++) {
+			dogFoodData += `<p id='dogFoodProducts'>product type: ${currentFood.types[j].type}</p>`;
+			// console.log("dog second for", currentFood);
 
 
-		for (var j = 0; j < data.dog_brands[i].types[j]; j++) {
-			dogFoodData += `<p>${currentFood.types[j].volumes}</p>`;
-			console.log(currentFood.types[j].volumes);
+			for (var k = 0; k < dogData.dog_brands[i].types[j].volumes.length; k++) {
+				dogFoodData += `<p id='dogFoodSize'>size: ${currentFood.types[j].volumes[k].name}</p>`;
+				dogFoodData += `<p id='dogFoodPrice'>price: ${currentFood.types[j].volumes[k].price}</p>`;
+				// console.log("dog third for", currentFood);
 
-			};
+				};
 			
 		};
 
 	};
+		contentEl.innerHTML += dogFoodData;
+		console.log("dog food", Date.now());
 };
 
 
 
+function catFoodLoaded() {
+	var catData = JSON.parse(this.responseText);
+
+	var catFoodData = "";
+	var currentFood = "";
+
+	for(var i = 0; i < catData.cat_brands.length; i++) {
+		currentFood = catData.cat_brands[i];
+		catFoodData += `<div class='catFoodOutput'>`;
+		catFoodData += `<h1 id='catBrandName'>${currentFood.name}</h1>`;
+		// console.log("cat first for", currentFood);
+
+
+		for (var j = 0; j < catData.cat_brands[i].types.length; j++) {
+			catFoodData += `<p id='catFoodProducts'>product type: ${currentFood.types[j].type}</p>`;
+			// console.log("cat second for", currentFood);
+
+
+			for (var k = 0; k < catData.cat_brands[i].types[j].volumes.length; k++) {
+				catFoodData += `<p id='catFoodSize'>size: ${currentFood.types[j].volumes[k].name}</p>`;
+				catFoodData += `<p id='catFoodPrice'>price: ${currentFood.types[j].volumes[k].price}</p>`;
+				// console.log("cat third for", currentFood);
+
+				};
+			
+		};
+
+	};
+		contentEl.innerHTML += catFoodData;
+		console.log("cat food", Date.now());
+};
+
 
 		
-		// contentEl.innerHTML = dogFoodData;
 
-var myRequest = new XMLHttpRequest();
+var dogRequest = new XMLHttpRequest();
 
-myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
-myRequest.addEventListener("error", executeThisCodeIfXHRFails);
-myRequest.open("GET", "dogfood.json");
-myRequest.send();
+dogRequest.addEventListener("load", dogFoodLoaded);
+dogRequest.addEventListener("error", dogFoodFails);
+dogRequest.open("GET", "dogfood.json");
+dogRequest.send();
 
-// console.log("Last line in the JavaScript file");
-// console.log(Date.now());
 
+var catRequest = new XMLHttpRequest();
+
+catRequest.addEventListener("load", catFoodLoaded);
+catRequest.addEventListener("error", catFoodFails);
+catRequest.open("GET", "catfood.json");
+catRequest.send();
+
+console.log("Last line in the JavaScript file");
+console.log(Date.now());
